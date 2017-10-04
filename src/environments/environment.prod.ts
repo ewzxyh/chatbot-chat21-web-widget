3e52fdba39b22524250ddd95d9a1c86287d9fb06
andrea leo <andrea.leo83@gmail.com>
2017-10-05 00:00:10 +0200

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: '[REDACTED_GOOGLE_KEY]',
+    authDomain: '<your-project-authdomain>',
+    databaseURL: 'https://bppmobileintranet-chat.firebaseio.com',
+    projectId: 'bppmobileintranet-chat',
+    storageBucket: '<your-storage-bucket>',
+    messagingSenderId: '478926705150'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,27 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: '[REDACTED_GOOGLE_KEY]',
+    authDomain: '<your-project-authdomain>',
+    databaseURL: 'https://chat21-web.firebaseio.com',
+    projectId: 'chat21-web',
+    storageBucket: '<your-storage-bucket>',
+    messagingSenderId: '680009090540'
+  }
+
+  // firebase: {
+  //   apiKey: '[REDACTED_GOOGLE_KEY]',
+  //   authDomain: '<your-project-authdomain>',
+  //   databaseURL: 'https://bppmobileintranet-chat.firebaseio.com',
+  //   projectId: 'bppmobileintranet-chat',
+  //   storageBucket: '<your-storage-bucket>',
+  //   messagingSenderId: '478926705150'
+  // }
+
+  
+  
 };
+
+
