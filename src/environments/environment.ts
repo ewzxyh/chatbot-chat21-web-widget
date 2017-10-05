15f071d99da41b17776e7c93db83b9b71b61cc5e
andrea leo <andrea.leo83@gmail.com>
2017-10-05 09:53:10 +0200

    changed firebase project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,11 +8,21 @@ export const environment = {
   firebase: {
     apiKey: '[REDACTED_GOOGLE_KEY]',
     authDomain: '<your-project-authdomain>',
-    databaseURL: 'https://chat21-web.firebaseio.com',
-    projectId: 'chat21-web',
+    databaseURL: 'https://ionic3chat.firebaseio.com',
+    projectId: 'ionic3chat',
     storageBucket: '<your-storage-bucket>',
-    messagingSenderId: '680009090540'
-  }
+    messagingSenderId: '1096415488178'
+  },
+  // firebase: {
+  //   apiKey: '[REDACTED_GOOGLE_KEY]',
+  //   authDomain: '<your-project-authdomain>',
+  //   databaseURL: 'https://chat21-web.firebaseio.com',
+  //   projectId: 'chat21-web',
+  //   storageBucket: '<your-storage-bucket>',
+  //   messagingSenderId: '680009090540'
+  // },
+  tenant: 'chat21',
+  agentId: '7bqPekKF9mO1cKHPZgNbj0JmANE2'
 
   // firebase: {
   //   apiKey: '[REDACTED_GOOGLE_KEY]',
