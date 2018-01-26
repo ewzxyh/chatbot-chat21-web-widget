a3b09275d9051f49572cace2acd6045ff2e06990
dariodepa <czone555@gmail.com>
2018-01-26 17:19:16 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: '[REDACTED_GOOGLE_KEY]',
-    authDomain: '<your-project-authdomain>',
-    databaseURL: 'https://ionic3chat.firebaseio.com',
-    projectId: 'ionic3chat',
-    storageBucket: '<your-storage-bucket>',
-    messagingSenderId: '1096415488178'
+    apiKey: '...',
+    authDomain: '...',
+    databaseURL: '...',
+    projectId: '...',
+    storageBucket: '...',
+    messagingSenderId: '...'
   },
   tenant: 'chat21',
   agentId: '9EBA3VLhNKMFIVa0IOco82TkIzk1'
