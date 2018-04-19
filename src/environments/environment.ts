db45440e9fd9ee4a8949e3678729cc7a5edd2419
alessandro-giovinazzo <alessandro.giovinazzo@frontiere21.it>
2018-04-19 19:24:18 +0200

    build 0.0.26 - https://docs.google.com/document/d/18iM6RLcyNujs-EtQHpBYfO_gGZGuvEBpaAy51HAg9mI/edit?usp=sharing

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  build: 'b.0.25',
+  build: 'b.0.26',
   firebase: {
     apiKey: '[REDACTED_GOOGLE_KEY]',
     authDomain: 'chat-v2-dev.firebaseapp.com',
