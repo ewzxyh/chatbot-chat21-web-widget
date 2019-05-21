b43b3d82018d47e194d757485356b7d249dcee2a
Dario De Pascalis <dariodepa75@gmail.com>
2019-05-21 11:05:25 +0200

    change css attributes.attachment; change url in getProjectById

--- src/app/providers/settings.service.ts
+++ src/app/providers/settings.service.ts
@@ -15,16 +15,18 @@ export class SettingsService {
 
   getProjectById(id: string): Observable<any[]> {
     // tslint:disable-next-line:max-line-length
-    const token = 'JWT [REDACTED_JWT]';
+    // const token = 'JWT [REDACTED_JWT]';
     const API_URL = this.appConfigService.getConfig().apiUrl;
-    const PROJECT_BASE_URL = API_URL + 'projects/';
-    const url = PROJECT_BASE_URL + id;
+    // const PROJECT_BASE_URL = API_URL + 'projects/';
+    // const url = PROJECT_BASE_URL + id;
+
+    const url = API_URL + id + '/widgets';
     console.log('!!! GET PROJECT BY ID URL', url);
     console.log('!!! GET ID', id);
-    console.log('!!! GET TOKEN', token);
+    // console.log('!!! GET TOKEN', token);
     const headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    headers.append('Authorization', token);
+    // headers.append('Authorization', token);
     return this.http
       .get(url, { headers })
       .map((response) => response.json());
