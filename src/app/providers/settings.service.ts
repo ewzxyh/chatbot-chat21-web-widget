59a34f036f70c9dee08409e553e48eafca8955ef
Dario De Pascalis <dariodepa75@gmail.com>
2019-06-19 18:02:33 +0200

    changes LocalSettingsService  add getProjectParametersById (for widows, departments, user_available)

--- src/app/providers/settings.service.ts
+++ src/app/providers/settings.service.ts
@@ -13,20 +13,12 @@ export class SettingsService {
     public authService: AuthService
   ) { }
 
-  getProjectById(id: string): Observable<any[]> {
-    // tslint:disable-next-line:max-line-length
-    // const token = 'JWT [REDACTED_JWT]';
-    const API_URL = this.appConfigService.getConfig().apiUrl;
-    // const PROJECT_BASE_URL = API_URL + 'projects/';
-    // const url = PROJECT_BASE_URL + id;
 
+  getProjectParametersById(id: string): Observable<any[]> {
+    const API_URL = this.appConfigService.getConfig().apiUrl;
     const url = API_URL + id + '/widgets';
-    console.log('!!! GET PROJECT BY ID URL', url);
-    console.log('!!! GET ID', id);
-    // console.log('!!! GET TOKEN', token);
     const headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    // headers.append('Authorization', token);
     return this.http
       .get(url, { headers })
       .map((response) => response.json());
